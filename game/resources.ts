3fe27fccdbb5a5580dedbea3ced442fd85857d7c
Alan Grgic <alan.grgic@gmail.com>
2016-04-16 15:52:28 -0500

    add bullets to resource file

--- game/resources.ts
+++ game/resources.ts
@@ -3,5 +3,8 @@ var Resources = {
    CircleSheildSheet: new ex.Texture('./img/circlesheild.png'),
    SquareSheildSheet: new ex.Texture('./img/squaresheild.png'),
    TriangleSheildSheet: new ex.Texture('./img/trianglesheild.png'),
-   PlayerBullet: new ex.Texture('./img/playerbullet.png')
+   PlayerBullet: new ex.Texture('./img/playerbullet.png'),
+   CircleBullet: new ex.Texture('./img/bullets/blueBullet.png'),
+   SquareBullet: new ex.Texture('./img/bullets/greenBullet.png'),
+   TriangleBullet: new ex.Texture('./img/bullets/yellowBullet.png')
 };
