ca0050afff82a0e2bafebbdc4e677e47d341be1b
Travis-CI <travis@excaliburjs.com>
2016-04-16 23:14:28 +0000

    Deploying game for 3fbfba86978eaafae4d33afa91bc244d88465e68

--- game/game.js
+++ game/game.js
@@ -50,6 +50,9 @@ var Resources = {
     SquareShieldSheet: new ex.Texture('./img/squaresheild.png'),
     TriangleShieldSheet: new ex.Texture('./img/trianglesheild.png'),
     PlayerBullet: new ex.Texture('./img/playerbullet.png'),
+    CircleBadguySheet: new ex.Texture('./img/circlebadguy.png'),
+    TriangleBadguySheet: new ex.Texture('./img/trianglebadguy.png'),
+    SquareBadguySheet: new ex.Texture('./img/squarebadguy.png'),
     CircleBullet: new ex.Texture('./img/bullets/blueBullet.png'),
     SquareBullet: new ex.Texture('./img/bullets/greenBullet.png'),
     TriangleBullet: new ex.Texture('./img/bullets/yellowBullet.png')
