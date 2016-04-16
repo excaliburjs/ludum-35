448da558ae48d679d91582d2875f07f1ea30d66a
Ante Grgic <ante16@gmail.com>
2016-04-16 17:40:38 -0500

    added bad guy images

--- game/resources.ts
+++ game/resources.ts
@@ -3,5 +3,8 @@ var Resources = {
    CircleShieldSheet: new ex.Texture('./img/circlesheild.png'),
    SquareShieldSheet: new ex.Texture('./img/squaresheild.png'),
    TriangleShieldSheet: new ex.Texture('./img/trianglesheild.png'),
-   PlayerBullet: new ex.Texture('./img/playerbullet.png')
+   PlayerBullet: new ex.Texture('./img/playerbullet.png'),
+   CircleBadguySheet: new ex.Texture('./img/circlebadguy.png'),
+   TriangleBadguySheet: new ex.Texture('./img/trianglebadguy.png'),
+   SquareBadguySheet: new ex.Texture('./img/squarebadguy.png')
 };
